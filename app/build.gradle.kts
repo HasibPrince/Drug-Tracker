8298c6f1f47316fe13120b47bd08776877fbe70d
HasibPrince <hasibprince314@gmail.com>
2025-05-03 12:50:47 +0600

    chore: target sdk reduced to 29 (Android 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     defaultConfig {
         applicationId = "com.hasib.drugtracker"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 29
         versionCode = 1
         versionName = "1.0"
 
